1e14430e53fdc7c48e1240a24a46a48e67739cf6
John Fitzgerald <usplitu@hotmail>
2016-08-02 17:22:09 +0100

    changed model residence id to string version uuid

--- app/models/Residence.java
+++ app/models/Residence.java
@@ -3,6 +3,7 @@ package models;
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -23,33 +24,43 @@ public class Residence extends GenericModel {
 	public double zoom;
 	public String photo;
 
+	public Residence() {
+	  id = UUID.randomUUID().toString();
+	  geolocation = "";
+	  date = 0L;
+	  rented = false;
+	  tenant = "";
+	  zoom = 0;
+	  photo = "";
+	}
+	
 	/**
 	 * Update this Residence but leave id unchanged
 	 * 
 	 * @param other
 	 */
-	public void update(Residence other) {
-		this.geolocation = other.geolocation;
-		this.date = other.date;
-		this.rented = other.rented;
-		this.tenant = other.tenant;
-		this.zoom = other.zoom;
-		this.photo = other.photo;
-	}
+//	public void update(Residence other) {
+//		this.geolocation = other.geolocation;
+//		this.date = other.date;
+//		this.rented = other.rented;
+//		this.tenant = other.tenant;
+//		this.zoom = other.zoom;
+//		this.photo = other.photo;
+//	}
 
 	/**
 	 * Copy constructor
 	 * @param other
 	 */
-	public Residence(Residence other) {
-		this.id = other.id;
-		this.geolocation = other.geolocation;
-		this.date = other.date;
-		this.rented = other.rented;
-		this.tenant = other.tenant;
-		this.zoom = other.zoom;
-		this.photo = other.photo;
-}
+//	public Residence(Residence other) {
+//		this.id = other.id;
+//		this.geolocation = other.geolocation;
+//		this.date = other.date;
+//		this.rented = other.rented;
+//		this.tenant = other.tenant;
+//		this.zoom = other.zoom;
+//		this.photo = other.photo;
+//    }
 	
 	public static Residence findById(String id) {
 		return find("id", id).first();
